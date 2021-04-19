39ddba6eb46f137caa8194bc47515d4d78ab673c
yemutbayevadina <deenaharvard@gmail.com>
2021-04-19 15:42:51 -0400

    adding solution

---
+++ fizzbazz.py
@@ -0,0 +1,14 @@
+
+for num in range(3):
+    num = int(input("Give me a number:"))
+    if num%3==0 and num%5==0:
+        print("Fizzbazz")
+    elif num%3==0:
+        print("Fizz")
+    elif num%5==0:
+        print("Buzz")
+
+
+
+
+
